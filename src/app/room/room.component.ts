a91b027c2a0c318d9442897a05d0cbb88ed98a14
Jussi Kinnula <jussi.kinnula@wunderdog.fi>
2017-05-08 16:55:01 +0300

    Convert room component tabs to spaces

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -5,76 +5,76 @@ import { IMessage, IRoom } from '../../models';
 import { MessageService } from './message.service';
 
 @Component({
-	selector: 'room',
-	styleUrls: ['./room.component.scss'],
-	templateUrl: './room.component.html'
+  selector: 'room',
+  styleUrls: ['./room.component.scss'],
+  templateUrl: './room.component.html'
 })
 
 export class RoomComponent implements OnInit, AfterViewInit, OnDestroy {
-	@ViewChild('scroll') private scroll: ElementRef;
-	@ViewChild('focus') private focus: ElementRef;
-	@Input() room: IRoom;
-	message: string = '';
-	messages: IMessage[];
-	private messageService: MessageService;
-	private alreadyLeftChannel: boolean = false;
+  @ViewChild('scroll') private scroll: ElementRef;
+  @ViewChild('focus') private focus: ElementRef;
+  @Input() room: IRoom;
+  message: string = '';
+  messages: IMessage[];
+  private messageService: MessageService;
+  private alreadyLeftChannel: boolean = false;
 
-	constructor(
-		private roomService: RoomService,
-		public userService: UserService,
-		public socketService: SocketService
-	) {}
+  constructor(
+    private roomService: RoomService,
+    public userService: UserService,
+    public socketService: SocketService
+  ) {}
 
-	// Handle keypress event, for saving nickname
-	ngOnInit(): void {
-		this.messageService = new MessageService(this.room.name);
-		this.messageService.messages.subscribe(messages => {
-			this.messages = messages;
-			setTimeout( () => {
-				this.scrollToBottom();
-			}, 200);
-		});
-		this.messageService.create(this.userService.nickname, 'joined the channel');
-	}
+  // Handle keypress event, for saving nickname
+  ngOnInit(): void {
+    this.messageService = new MessageService(this.room.name);
+    this.messageService.messages.subscribe(messages => {
+      this.messages = messages;
+      setTimeout( () => {
+        this.scrollToBottom();
+      }, 200);
+    });
+    this.messageService.create(this.userService.nickname, 'joined the channel');
+  }
 
-	// After view initialized, focus on chat message text input
-	ngAfterViewInit(): void {
-		this.focus.nativeElement.focus();
-	}
+  // After view initialized, focus on chat message text input
+  ngAfterViewInit(): void {
+    this.focus.nativeElement.focus();
+  }
 
-	// When component is destroyed, ensure that leave message is sent
-	ngOnDestroy(): void {
-		if (!this.alreadyLeftChannel) {
-			this.leave();
-		}
-	}
+  // When component is destroyed, ensure that leave message is sent
+  ngOnDestroy(): void {
+    if (!this.alreadyLeftChannel) {
+      this.leave();
+    }
+  }
 
-	// Send chat message, and reset message text input
-	send(): void {
-		this.messageService.create(this.userService.nickname, this.message);
-		this.message = '';
-	}
+  // Send chat message, and reset message text input
+  send(): void {
+    this.messageService.create(this.userService.nickname, this.message);
+    this.message = '';
+  }
 
-	// Leave room gracefully
-	leave(): void {
-		this.alreadyLeftChannel = true;
-		this.messageService.create(this.userService.nickname, 'left the channel');
-		this.roomService.leave(this.room.name);
-	}
+  // Leave room gracefully
+  leave(): void {
+    this.alreadyLeftChannel = true;
+    this.messageService.create(this.userService.nickname, 'left the channel');
+    this.roomService.leave(this.room.name);
+  }
 
-	//* Scroll to bottom (this is called when new message is received)
-	scrollToBottom(): void {
-		try {
-			this.scroll.nativeElement.scrollTop = this.scroll.nativeElement.scrollHeight;
-		} catch(error) {
-			console.log('ERROR:', error);
-		}
-	}
+  //* Scroll to bottom (this is called when new message is received)
+  scrollToBottom(): void {
+    try {
+      this.scroll.nativeElement.scrollTop = this.scroll.nativeElement.scrollHeight;
+    } catch(error) {
+      console.log('ERROR:', error);
+    }
+  }
 
-	// Handle keypress event, for sending chat message
-	eventHandler(event: KeyboardEvent): void {
-		if (event.key === 'Enter') {
-			this.send();
-		}
-	}
+  // Handle keypress event, for sending chat message
+  eventHandler(event: KeyboardEvent): void {
+    if (event.key === 'Enter') {
+      this.send();
+    }
+  }
 }
